5c10160cf345dc027c76e199605c143a1f0505bc
RasmusEng <70199682+RasmusEng@users.noreply.github.com>
2024-01-16 22:20:20 +0100

    das

--- Labb-1/Labb-1/src/LifeLength.java
+++ Labb-1/Labb-1/src/LifeLength.java
@@ -10,4 +10,41 @@ public class LifeLength {
             return a0*3 +1;
         }
     }
+
+    public static int counter(int a0, int steps){
+        int count = 0;
+        do{
+            a0 = f1(a0);
+            count++;
+        }while(count < steps);
+        return a0;
+    }
+
+    public static int f2(int a0){
+        int n = 2;
+        int step = counter(a0, n);
+        return step;
+    }
+    public static int f4(int a0){
+        int n = 4;
+        int step = counter(a0, n);
+        return step;
+    }
+    public static int f8(int a0) {
+        int n = 8;
+        int step = counter(a0, n);
+        return step;
+
+    }
+    public static int f16(int a0){
+        int n = 16;
+        int step = counter(a0, n);
+        return step;
+    }
+    public static int f32(int a0){
+        int n = 32;
+        int step = counter(a0, n);
+        return step;
+    }
+
 }
--- Labb-1/Labb-1/src/Main.java
+++ Labb-1/Labb-1/src/Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Task1();
+        Task2();
+    }
+
+    public static void Task1(){
         // Skapar scannern
         Scanner scan = new Scanner(System.in);
 
@@ -20,14 +25,43 @@ public class Main {
                 break;
             }
 
-            int count = 0; // Räknar antalet varv i loopen till a0 når 1
-            // Kör loopen en gång innan vi kollar while kravet.
-            do {
             a0 = LifeLength.f1(a0);
-            //System.out.println(a0);
-            count++;
-            } while(a0 != 1);
-            System.out.println(count);
+            System.out.println(a0);
+
+
+        }
+        scan.close();
+    }
+
+    public static void Task2(){
+        Scanner scan = new Scanner(System.in);
+        while (true){
+            int a0 = 0;
+
+            // Tar en input och testar om det är ett heltal som är större eller likamed 0
+            try {
+                System.out.print("Skriv in ett heltal större än noll: ");
+                a0 = scan.nextInt();
+            } catch (Exception e) {
+                System.out.println("Fel: Ej ett heltal > 0");
+            }
+            if(a0 == 0){
+                System.out.println("Avslutar programmet");
+                break;
+            }
+
+
+            int f2= LifeLength.f2(a0);
+            int f4= LifeLength.f4(a0);
+            int f8= LifeLength.f8(a0);
+            int f16= LifeLength.f16(a0);
+            int f32= LifeLength.f32(a0);
+            System.out.print("f1=" + a0 + "\t");
+            System.out.print("f2=" + f2+ "\t");
+            System.out.print("f4=" + f4+ "\t");
+            System.out.print("f8=" + f8+ "\t");
+            System.out.print("f16=" + f16+ "\t");
+            System.out.println("f32=" + f32);
 
         }
         scan.close();
