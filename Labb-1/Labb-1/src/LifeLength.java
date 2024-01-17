f2da4bc437ed1bf7e28404e428f92f8d59a49fc3
RasmusEng <rasmus.engstrom@hotmail.com>
2024-01-17 16:13:17 +0100

    hej

--- Labb-1/Labb-1/src/LifeLength.java
+++ Labb-1/Labb-1/src/LifeLength.java
@@ -1,50 +1,71 @@
+import com.sun.source.tree.BreakTree;
+
 public class LifeLength {
-    public static int f1(int a0){
-        if(a0 <= 1){
+    public static int f1(int a0) {
+        if (a0 <= 1) {
             return a0;
         }
-        if(a0 % 2 == 0){
-            return a0/2;
-        }
-        else{
-            return a0*3 +1;
+        if (a0 % 2 == 0) {
+            return a0 / 2;
+        } else {
+            return a0 * 3 + 1;
         }
     }
 
-    public static int counter(int a0, int steps){
-        int count = 0;
-        do{
-            a0 = f1(a0);
-            count++;
-        }while(count < steps);
-        return a0;
+    public static int f1t2(int a0, int n) {
+        if (n == 0){
+            return a0;
+        }else{
+            if (a0 == 1) {
+                return f1t2(a0, n-1);
+            }
+            if (a0 % 2 == 0) {
+                return f1t2(a0 / 2, n-1);
+            } else {
+                return f1t2((a0 * 3 + 1), n-1);
+            }
+        }
     }
 
     public static int f2(int a0){
-        int n = 2;
-        int step = counter(a0, n);
-        return step;
-    }
-    public static int f4(int a0){
-        int n = 4;
-        int step = counter(a0, n);
-        return step;
-    }
-    public static int f8(int a0) {
-        int n = 8;
-        int step = counter(a0, n);
-        return step;
-
-    }
-    public static int f16(int a0){
-        int n = 16;
-        int step = counter(a0, n);
-        return step;
-    }
-    public static int f32(int a0){
-        int n = 32;
-        int step = counter(a0, n);
-        return step;
+        return f1t2(a0, 2);
+    }public static int f4(int a0){
+        return f1t2(a0, 4);
+    }public static int f8(int a0){
+        return f1t2(a0, 8);
+    }public static int f16(int a0){
+        return f1t2(a0, 16);
+    }public static int f32(int a0){
+        return f1t2(a0, 32);
+    }public static int f64(int a0){
+        return f1t2(a0, 64);
+    }public static int f128(int a0){
+        return f1t2(a0, 64);
     }
 
+    public static int iteradeF(int a0, int n) {
+        for (int i = 0; i < n; i++) {
+           a0 = f1(a0);
+        }
+        return a0;
+    }
+    public static int iterLifeLength(int a0){
+        int count = 0;
+        while(a0 != 1){
+            a0 = f1(a0);
+            count++;
+        }
+        return count;
+    }
+
+    public static int recLifeLength(int a0){
+        if (a0 <= 1){
+            return 0;
+        }
+        else {
+            return 1 + recLifeLength(f1(a0));
+        }
+
+    }
 }
+
--- Labb-1/Labb-1/src/Main.java
+++ Labb-1/Labb-1/src/Main.java
@@ -2,68 +2,73 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Task1();
-        Task2();
-    }
-
-    public static void Task1(){
         // Skapar scannern
         Scanner scan = new Scanner(System.in);
 
         while (true){
             int a0 = 0;
+            int n = 0;
 
             // Tar en input och testar om det är ett heltal som är större eller likamed 0
             try {
                 System.out.print("Skriv in ett heltal större än noll: ");
                 a0 = scan.nextInt();
+
+                if(a0 == 0){
+                    System.out.println("Avslutar programmet");
+                    break;
+                }
+                //Task 3
+               /* System.out.print("Skriv in antal steg: ");
+                n = scan.nextInt();*/
             } catch (Exception e) {
                 System.out.println("Fel: Ej ett heltal > 0");
             }
-            if(a0 == 0){
-                System.out.println("Avslutar programmet");
-                break;
-            }
 
-            a0 = LifeLength.f1(a0);
-            System.out.println(a0);
-
-
+            //Task1(a0);
+            //Task2(a0);
+            //Task3(a0, n);
+            //Task4(a0);
+            Task6(a0);
         }
         scan.close();
     }
 
-    public static void Task2(){
-        Scanner scan = new Scanner(System.in);
-        while (true){
-            int a0 = 0;
-
-            // Tar en input och testar om det är ett heltal som är större eller likamed 0
-            try {
-                System.out.print("Skriv in ett heltal större än noll: ");
-                a0 = scan.nextInt();
-            } catch (Exception e) {
-                System.out.println("Fel: Ej ett heltal > 0");
-            }
-            if(a0 == 0){
-                System.out.println("Avslutar programmet");
-                break;
-            }
-
-
-            int f2= LifeLength.f2(a0);
-            int f4= LifeLength.f4(a0);
-            int f8= LifeLength.f8(a0);
-            int f16= LifeLength.f16(a0);
-            int f32= LifeLength.f32(a0);
-            System.out.print("f1=" + a0 + "\t");
-            System.out.print("f2=" + f2+ "\t");
-            System.out.print("f4=" + f4+ "\t");
-            System.out.print("f8=" + f8+ "\t");
-            System.out.print("f16=" + f16+ "\t");
-            System.out.println("f32=" + f32);
-
-        }
-        scan.close();
+    public static void Task1(int a0){
+        a0 = LifeLength.f1(a0);
+        System.out.println(a0);
     }
+    public static void Task2(int a0){
+        int f1= LifeLength.f1t2(a0, 1);
+        int f2= LifeLength.f2(a0);
+        int f4= LifeLength.f4(a0);
+        int f8= LifeLength.f8(a0);
+        int f16= LifeLength.f16(a0);
+        int f32= LifeLength.f32(a0);
+        int f64= LifeLength.f64(a0);
+        int f128= LifeLength.f128(a0);
+        System.out.print("f1=" + f1 + "\t");
+        System.out.print("f2=" + f2+ "\t");
+        System.out.print("f4=" + f4+ "\t");
+        System.out.print("f8=" + f8+ "\t");
+        System.out.print("f16=" + f16+ "\t");
+        System.out.print("f32=" + f32 + "\t");
+        System.out.print("f64=" + f64 + "\t");
+        System.out.println("f128=" + f128);
+
+    }
+    public static void Task3(int a0, int n){
+        a0 = LifeLength.iteradeF(a0, n);
+        System.out.println("Vid steg " + n +" är värdet = " + a0);
+    }
+    public static void Task4(int a0){
+        int steps = LifeLength.iterLifeLength(a0);
+        System.out.println("The life length of " + a0 + " is " + steps + ".");
+    }
+
+    public static void Task6(int a0){
+        int steps = LifeLength.recLifeLength(a0);
+        System.out.println("Steg =" + steps);
+    }
+
 }
