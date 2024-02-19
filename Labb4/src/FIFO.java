de854a7b6b3d2424a379e201f8305f31d2fce234
RasmusEng <rasmus.engstrom@hotmail.com>
2024-02-19 18:33:32 +0100

    hah

--- Labb4/src/FIFO.java
+++ Labb4/src/FIFO.java
@@ -1,5 +1,79 @@
-import sun.misc.Queue;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
-public class FIFO extends Queue {
+public class FIFO implements Queue {
+    ArrayList<Object> queue = new ArrayList<Object>();
+    int maxSize = 0;
+    @Override
+    public void add(Object o) {
+        queue.add(o);
+        if(queue.size() >= maxSize){
+            maxSize++;
+        }
+    }
 
+    @Override
+    public void removeFirst(){
+        if(queue.size() == 0){
+            throw new NoSuchElementException();
+        }
+            queue.remove(0);
+    }
+
+    @Override
+    public Object first(){
+        if(queue.size() > 0){
+            return queue.get(0);
+        }
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public int maxSize() {
+        return maxSize;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if(queue.size() != 0){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return queue.size();
+    }
+
+    public String toString(){
+        String stringQueue = "Queue: ";
+        for(int i = 0; i < queue.size(); i++){
+            stringQueue += "(" + String.valueOf(queue.get(i)) + ") ";
+        }
+        return stringQueue;
+    }
+
+    public boolean equals(Object o){
+        if(!(o instanceof FIFO)){
+            throw new ClassCastException();
+        }
+        //((FIFO)o) betyder att man typecastar o till en FIFO
+        if(queue.size() != ((FIFO)o).size()){
+            return false;
+        }
+        // Loopar igenom queue och o.queue och kollar om varje element är lika
+        for(int i = 0; i < queue.size(); i++){
+            if(queue.get(i) == null){
+                if(((FIFO)o).queue.get(i) != null){
+                    return false;
+                }
+            }else {
+                if(!(queue.get(i).equals(((FIFO)o).queue.get(i)))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
---
+++ Labb4/src/GraphIO.java
@@ -0,0 +1,19 @@
+import java.util.NoSuchElementException;
+
+public class GraphIO implements Graph {
+
+    @Override
+    public void addNode(int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void addEdge(int i, int i1, int i2) throws NoSuchElementException {
+
+    }
+
+    static public void readFile(Graph g, String fileName){
+
+    }
+
+}
