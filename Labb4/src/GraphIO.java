fa07e5a27ca7be3dc4501ac15b15a4f6e902a4e5
RasmusEng <70199682+RasmusEng@users.noreply.github.com>
2024-02-19 20:32:44 +0100

    yay

--- Labb4/src/GraphIO.java
+++ Labb4/src/GraphIO.java
@@ -1,18 +1,31 @@
-import java.util.NoSuchElementException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class GraphIO implements Graph {
+public class GraphIO{
 
-    @Override
-    public void addNode(int i, int i1, int i2) {
+    static public void readFile(Graph g, String fileName) throws IOException {
+        try {
+            File filen = new File(fileName);
+            Scanner scanner = new Scanner(filen);
+            int node = scanner.nextInt();
+            for(int i = 0; i < node; i++){
+                g.addNode(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+            }
 
-    }
+            try{
+                while(scanner.hasNextLine()){
+                    g.addEdge(scanner.nextInt(),scanner.nextInt(),scanner.nextInt());
+                }
+            }catch(Exception e){
+                scanner.close();
+            }
 
-    @Override
-    public void addEdge(int i, int i1, int i2) throws NoSuchElementException {
+        }catch (FileNotFoundException e){
+            throw new IOException();
+        }
 
-    }
-
-    static public void readFile(Graph g, String fileName){
 
     }
 
